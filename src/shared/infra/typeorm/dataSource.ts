68afd72ffb2a22b924e40532f48414f9aed06c4f
Victhor Araújo <victhor.araujo.41@gmail.com>
2022-04-08 16:24:33 -0300

    clear on env params

--- src/shared/infra/typeorm/dataSource.ts
+++ src/shared/infra/typeorm/dataSource.ts
@@ -5,11 +5,6 @@ import { DataSource } from 'typeorm';
 export const dataSource = new DataSource({
   type: 'postgres',
   url: process.env.DATABASE_URL,
-  // host: process.env.DATABASE_HOST,
-  // port: 5432,
-  // username: process.env.DATABASE_USER,
-  // password: process.env.DATABASE_PASSWORD,
-  // database: process.env.DATABASE_DB,
   migrations: [
     process.env.NODE_ENV === 'prod'
       ? 'dist/shared/infra/typeorm/migrations/*.js'
